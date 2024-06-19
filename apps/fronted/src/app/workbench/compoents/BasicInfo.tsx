d228c9bc0461c3486f90a89e517c0b64109a1ecb
yuguangzhou <yuguangzhou@yiwise.com>
2024-06-19 23:46:46 +0800

    feat: basic-info

--- apps/fronted/src/app/workbench/compoents/BasicInfo.tsx
+++ apps/fronted/src/app/workbench/compoents/BasicInfo.tsx
@@ -1,5 +1,13 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const BasicInfo = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center space-x-2">
+      <Label htmlFor="terms">姓名</Label>
+      <Input id="terms" className="w-[200px]" />
+    </div>
+  );
 };
 
 export default BasicInfo;

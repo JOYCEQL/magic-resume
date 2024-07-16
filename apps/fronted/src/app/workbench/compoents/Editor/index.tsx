531c93f23e1de9bcdadd18897578a3c468b0921a
yuguangzhou <yuguangzhou@yiwise.com>
2024-07-16 23:55:21 +0800

    feat: scroll into view

--- apps/fronted/src/app/workbench/compoents/Editor/index.tsx
+++ apps/fronted/src/app/workbench/compoents/Editor/index.tsx
@@ -1,5 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import BasicInfo from "../BasicInfo";
 import Skills from "../Skills";
 import Project from "../Project";
@@ -16,36 +16,49 @@ import {
 } from "lucide-react";
 const Editor = () => {
   const [activeTab, setActiveTab] = useState("basic");
+  const basicRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectRef = useRef(null);
+  const educationRef = useRef(null);
+  const certRef = useRef(null);
+  const empolymentRef = useRef(null);
+
   const tabList = [
     {
       value: "basic",
       label: "基本信息",
-      icon: <UserRound size={16} />
+      icon: <UserRound size={16} />,
+      ref: basicRef
     },
     {
       value: "skills",
       label: "专业技能",
-      icon: <PencilRuler size={16} />
+      icon: <PencilRuler size={16} />,
+      ref: skillsRef
     },
     {
       value: "project",
       label: "项目经历",
-      icon: <FileJson size={16} />
+      icon: <FileJson size={16} />,
+      ref: projectRef
     },
     {
       value: "empolyment",
       label: "工作经历",
-      icon: <Network size={16} />
+      icon: <Network size={16} />,
+      ref: empolymentRef
     },
     {
       value: "education",
       label: "教育经历",
-      icon: <GraduationCap size={16} />
+      icon: <GraduationCap size={16} />,
+      ref: educationRef
     },
     {
       value: "cert",
       label: "技能证书",
-      icon: <BookOpen size={16} />
+      icon: <BookOpen size={16} />,
+      ref: certRef
     }
   ];
   return (
@@ -56,6 +69,13 @@ const Editor = () => {
             <div
               key={index}
               className="flex flex-col items-center text-[12px] p-[12px] cursor-pointer hover:bg-[#e3e3e5] hover:rounded-[4px]"
+              onClick={() => {
+                item.ref.current?.scrollIntoView({
+                  behavior: "smooth",
+                  block: "end",
+                  inline: "nearest"
+                });
+              }}
             >
               <div>{item.icon}</div>
               <div>{item.label}</div>
@@ -70,17 +90,29 @@ const Editor = () => {
         <div className="text-[24px] mb-[10px]">前端-xx-x年</div>
 
         <div className="mt-[12px]">
-          <div className="text-[20px] mb-[12px]">基本信息</div>
+          <div className="text-[20px] mb-[12px]" ref={basicRef}>
+            基本信息
+          </div>
           <BasicInfo></BasicInfo>
-          <div className="text-[20px] mb-[12px]">专业技能</div>
+          <div className="text-[20px] mb-[12px]" ref={skillsRef}>
+            专业技能
+          </div>
           <Skills></Skills>
-          <div className="text-[20px] mb-[12px]">项目经历</div>
+          <div className="text-[20px] mb-[12px]" ref={projectRef}>
+            项目经历
+          </div>
           <Project></Project>
-          <div className="text-[20px] mb-[12px]">工作经历</div>
+          <div ref={empolymentRef} className="text-[20px] mb-[12px]">
+            工作经历
+          </div>
           <Empolyment></Empolyment>
-          <div className="text-[20px] mb-[12px]">教育经历</div>
+          <div ref={educationRef} className="text-[20px] mb-[12px]">
+            教育经历
+          </div>
           <Education></Education>
-          <div className="text-[20px]">技能证书</div>
+          <div ref={certRef} className="text-[20px]">
+            技能证书
+          </div>
           <Cert></Cert>
         </div>
       </div>

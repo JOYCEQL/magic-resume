3c2aa6d22aed1fab750084535b51446774a25737
siyue <github-actions[bot]@users.noreply.github.com>
2024-10-25 23:36:04 +0800

    fix: build error

--- apps/fronted/postcss.config.mjs
+++ apps/fronted/postcss.config.mjs
@@ -3,8 +3,7 @@ const config = {
   plugins: {
     "postcss-normalize": {
       // 配置选项
-      allowDuplicates: false, // 不允许重复导入
-      forceImport: true // 强制导入所有规范化样式
+      allowDuplicates: false // 不允许重复导入
     },
     tailwindcss: {}
   }

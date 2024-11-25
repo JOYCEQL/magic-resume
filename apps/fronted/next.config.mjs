01219dafaace4a209b133821c158bd9010567774
JOYCEQL <1449239013@qq.com>
2024-11-25 20:52:29 +0800

    fix: pdf font render

--- apps/fronted/next.config.mjs
+++ apps/fronted/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true

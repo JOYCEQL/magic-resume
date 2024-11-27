8175e95c3f58163b2684b1924532821062e7297a
JOYCEQL <1449239013@qq.com>
2024-11-27 15:01:02 +0800

    fix: pdf font

--- apps/fronted/src/app/api/generate-pdf/route.ts
+++ apps/fronted/src/app/api/generate-pdf/route.ts
@@ -33,6 +33,7 @@ export async function POST(req: Request) {
             font-display: swap;
           }
         </style>
+        <style> ${styles} </style>
       </head>
       <body>
         ${content}

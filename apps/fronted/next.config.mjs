e7fe63e9d84f2af1f4d9446911aa2bc3e55a0ecd
JOYCEQL <1449239013@qq.com>
2024-12-01 00:24:06 +0800

    feat: pdf exoport use tecent scf

--- apps/fronted/next.config.mjs
+++ apps/fronted/next.config.mjs
@@ -2,11 +2,18 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/generate-pdf",
+        destination:
+          "http://1255612844-0z3iovadu8.ap-chengdu.tencentscf.com/generate-pdf"
+      }
+    ];
   }
 };
 

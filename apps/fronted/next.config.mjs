9f612fd2bb5cbdacb63bc4eb41832fc31a6ba6a2
JOYCEQL <1449239013@qq.com>
2024-12-01 00:24:06 +0800

    feat: pdf exoport use tecent scf

--- apps/fronted/next.config.mjs
+++ apps/fronted/next.config.mjs
@@ -12,10 +12,6 @@ const nextConfig = {
         source: "/generate-pdf",
         destination:
           "http://1255612844-0z3iovadu8.ap-chengdu.tencentscf.com/generate-pdf"
-      },
-      {
-        source: "/:path*",
-        destination: "/"
       }
     ];
   }

7fdbe0b5af903f1205e9cf4bd8b6c96dba64634e
JOYCEQL <1449239013@qq.com>
2025-01-13 00:21:04 +0800

    perf: seo google config

--- apps/fronted/src/app/layout.tsx
+++ apps/fronted/src/app/layout.tsx
@@ -1,8 +1,24 @@
 import { ReactNode } from "react";
+import { Metadata } from "next";
 import "./globals.css";
 
 type Props = {
   children: ReactNode;
+};
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://magicv.art"),
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }: Props) {

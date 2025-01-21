82a23a4752983d174a270feadeabe95e133255d1
JOYCEQL <1449239013@qq.com>
2025-01-22 00:17:08 +0800

    feat: sitemap config & chinese title change

--- apps/fronted/src/app/sitemap.ts
+++ apps/fronted/src/app/sitemap.ts
@@ -1,18 +1,14 @@
-import type { MetadataRoute } from "next";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://magicv.art/",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://magicv.art/zh",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://magicv.art/en",
-      lastModified: new Date(),
-    },
-  ];
+  const baseUrl = "https://magicv.art/";
+
+  const routes = ["", "zh", "en"];
+
+  const sitemap: MetadataRoute.Sitemap = routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+  }));
+
+  return sitemap;
 }

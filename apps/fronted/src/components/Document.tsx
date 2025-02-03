f24557fdb12233149a93205d0e2630ae215cf9e2
siyue <github-actions[bot]@users.noreply.github.com>
2025-02-04 00:47:50 +0800

    fix: favicon.ico

--- apps/fronted/src/components/Document.tsx
+++ apps/fronted/src/components/Document.tsx
@@ -15,7 +15,7 @@ export default function Document({ children, locale, bodyClassName }: Props) {
   return (
     <html className={inter.className} lang={locale} suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico?v=1" />
+        <link rel="icon" href="/favicon.ico?v=2" />
       </head>
       <body className={bodyClassName}>{children}</body>
     </html>

b8bfa060995c829c4acfb5c99423d041256447b4
siyue <github-actions[bot]@users.noreply.github.com>
2025-02-03 20:12:26 +0800

    fix: favicon.ico cache

--- apps/fronted/src/components/Document.tsx
+++ apps/fronted/src/components/Document.tsx
@@ -14,6 +14,9 @@ type Props = {
 export default function Document({ children, locale, bodyClassName }: Props) {
   return (
     <html className={inter.className} lang={locale} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico?v=1" />
+      </head>
       <body className={bodyClassName}>{children}</body>
     </html>
   );

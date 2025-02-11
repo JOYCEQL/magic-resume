4deb61b074e78024f4f7433f163f66198fa13d19
JOYCEQL <1449239013@qq.com>
2025-02-11 10:02:41 +0800

    chore: sitemap.xml remove /

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://magicv.art/";
 
-  const routes = ["", "zh", "en"];
+  const routes = ["zh", "en"];
 
   const sitemap: MetadataRoute.Sitemap = routes.map((route) => ({
     url: `${baseUrl}${route}`,

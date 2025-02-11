a3c54cc8cbee947b6b4de37f00fd15562d3eb387
JOYCEQL <1449239013@qq.com>
2025-02-11 14:28:36 +0800

    build: docker config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const withNextIntl = createNextIntlPlugin();
 
@@ -7,6 +9,7 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: "standalone",
   async rewrites() {
     return [
       {

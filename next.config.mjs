66ece289284bf4154e0b3d9d753883b539498d33
JOYCEQL <1449239013@qq.com>
2025-03-05 22:25:53 +0800

    feat: update PDF generation endpoint to use direct URL and enable CORS

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,6 @@ const config = {
     ignoreBuildErrors: true,
   },
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/generate-pdf",
-        destination:
-          "http://1255612844-0z3iovadu8.ap-chengdu.tencentscf.com/generate-pdf",
-      },
-    ];
-  },
 };
 
 export default withNextIntl(config);

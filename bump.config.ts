950a328a7f3af4651c74a199187b620300c03503
JOYCEQL <1449239013@qq.com>
2026-02-28 22:38:27 +0800

    ci: bumpp and release

--- bump.config.ts
+++ bump.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'bumpp'
 
 export default defineConfig({
-  execute: 'npx changelogen --output CHANGELOG.md',
+  execute: 'npx changelogen --output CHANGELOG.md && node -e "const fs=require(\'fs\'); const pkg=require(\'./package.json\'); let c=fs.readFileSync(\'CHANGELOG.md\',\'utf8\'); c=c.replace(/## v[\\d.]+...main/g, \'## v\' + pkg.version); fs.writeFileSync(\'CHANGELOG.md\', c);"',
   commit: true,
   tag: true,
   push: false,
